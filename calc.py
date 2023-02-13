302f5b3cd8866d3ab456787165dbe0a0f9c27302
Zaid kamil <xaidmetamorphos@gmail.com>
2023-02-13 12:33:33 +0530

    match case

--- calc.py
+++ calc.py
@@ -7,28 +7,29 @@ while True:
     print('3. Multiplication')
     print('4. Division')
     print('5. Exit')
-    ch = input('Enter your choice: ')
-    if ch == '1':
-        a = int(input('Enter first number: '))
-        b = int(input('Enter second number: '))
-        print(f'{a} + {b} is {a+b}')
-    elif ch == '2':
-        a = int(input('Enter first number: '))
-        b = int(input('Enter second number: '))
-        print(f'{a} - {b} is {a-b}')
-    elif ch == '3':
-        a = int(input('Enter first number: '))
-        b = int(input('Enter second number: '))
-        print(f'{a} * {b} is {a*b}')
-    elif ch == '4':
-        a = int(input('Enter first number: '))
-        b = int(input('Enter second number: '))
-        print(f'{a} * {b} is {a/b}')
-    elif ch == '5':
-        print('Exiting...')
-        break
-    else:
-        print('Invalid choice')
+    match(input('Enter your choice: ')):
+        case '1':
+            a = int(input('Enter first number: '))
+            b = int(input('Enter second number: '))
+            print(f'{a} + {b} is {a+b}')
+        case '2':
+            a = int(input('Enter first number: '))
+            b = int(input('Enter second number: '))
+            print(f'{a} - {b} is {a-b}')
+        case '3':
+            a = int(input('Enter first number: '))
+            b = int(input('Enter second number: '))
+            print(f'{a} * {b} is {a*b}')
+        case '4':
+            a = int(input('Enter first number: '))
+            b = int(input('Enter second number: '))
+            print(f'{a} * {b} is {a/b}')
+        case '5':
+            print('Exiting...')
+            break
+        case _:
+            print('Invalid choice')
+            
     input('Press enter to continue...')
     os.system('cls')
     1 / 23
