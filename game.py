f2049d9137c3472b5a8ea311715ea1828e7ed04c
Zaid Kamil <xaidmetamorphos@gmail.com>
2023-02-20 12:34:11 +0530

    func #2

--- game.py
+++ game.py
@@ -3,12 +3,12 @@ import pgzrun
 music.play('bgm')
 music.set_volume(0.3)
 
-b = Rect((50,50), (50,25))
+b = Actor('logo2', (50,50))
 vx, vy = 3, 2
 
 def draw():
     screen.fill('white')
-    screen.draw.filled_rect(b, 'red')
+    b.draw()
 
 def update():
     global vx, vy
@@ -16,7 +16,7 @@ def update():
     b.y += vy
     if b.right > 800 or b.left < 0:
         vx = -vx
-        sounds.s1.play()
+        sounds.s2.play()
     if b.bottom > 600 or b.top < 0:
         vy = -vy
         sounds.s1.play()
